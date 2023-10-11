6f36fd21cd7d411f2946c517a7f7b1947e4a018f
Eric Noriaki Yaguchi <85816525+norigushi@users.noreply.github.com>
2023-10-11 01:48:42 -0300

    gera.cpp

--- saet23/Empilha copo/gera.cpp
+++ saet23/Empilha copo/gera.cpp
@@ -3,18 +3,11 @@ using namespace std;
 
 int main()
 {
-    char s;
     srand(time(NULL));
-    int y = 1 + (rand() % 10000);
-    cout<< y<<endl;
-    for (int j = 0; j < y; j++)
+    int x = 1 + ( rand() % 99 );
+    cout<<x<<endl;
+    for( int i =0 ;i<x; i++)
     {
-        int x = 1 + (rand() % 20);
-        for (int i = 0; i < x; i++)
-        {
-            s = rand() % 26 + 65 + rand() % 2 * 32;
-            cout << s;
-        }
-        cout<<endl;
+        cout<< 1 + (rand()%10000)<<endl;
     }
 }
